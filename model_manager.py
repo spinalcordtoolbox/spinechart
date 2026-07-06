d0359a7b8b5dc814bc1103f9b4340a29e62a80c8
Leana Yu <leana.yu0@gmail.com>
2026-07-06 10:19:10 -0400

    added progress bar for model download

--- model_manager.py
+++ model_manager.py
@@ -7,7 +7,7 @@ from the corresponding GitHub release and extract them.
 
 from pathlib import Path
 import zipfile
-
+from tqdm import tqdm
 import requests
 
 # Update this for each release
@@ -23,30 +23,43 @@ MODEL_FILE = Path("output/predictions/centile_curves_solidity.parquet")
 
 def ensure_models() -> None:
     """
-    Ensure that the precomputed model files are available.
+    Ensure that the precomputed model and prediction files are available.
 
-    If the models do not exist, download and extract the precomputed models from the GitHub release.
+    If the models and predictions files do not exist, download and extract the precomputed ones from the GitHub release.
     """
     if MODEL_FILE.exists():
         return
 
-    print("Precomputed models not found.")
-    print("Downloading models...")
+    print("Precomputed models and predictions not found.")
+    print("Downloading models and predictions...")
 
     zip_path = Path("output.zip")
 
+    # Download the ZIP file in chunks while displaying a progress bar
     response = requests.get(MODEL_URL, stream=True)
     response.raise_for_status()
+    
+    total_size = int(response.headers.get("content-length", 0))
 
-    with zip_path.open("wb") as f:
+    with (zip_path.open("wb") as f,
+          tqdm(
+            total=total_size,
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            desc="Downloading models and preds",
+        ) as pbar,
+    ):
         for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
+            if chunk:
+                f.write(chunk)
+                pbar.update(len(chunk))
 
-    print("Extracting models...")
+    print("Extracting...")
 
     with zipfile.ZipFile(zip_path, "r") as z:
         z.extractall(".")
 
     zip_path.unlink()
 
-    print("Models downloaded successfully.")
+    print("Models and predictions downloaded successfully.")
